f49fcdc0344f5d23a356942ad744679bf354b8e7
Felipe López <flh.1989@gmail.com>
2018-11-03 15:51:06 -0600

    m

---
+++ setup.py
@@ -0,0 +1,27 @@
+import io
+import re
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with io.open('minpkg/__init__.py', 'rt', encoding='utf8') as f:
+    version = re.search(r'__version__ = \'(.*?)\'', f.read()).group(1)
+
+setuptools.setup(
+    name="minpkg",
+    version=version,
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
